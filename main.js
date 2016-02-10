0e684ff888ae24bb0f9a3256937ed891e4b01896
rain84 <rainy_sky@mail.ru>
2016-02-11 00:24:19 +0300

    Main.js updated

--- main.js
+++ main.js
@@ -8,23 +8,23 @@
 
 
 (function ( patterns ) {
-
+	
 	$( main );
-
+	
 	function main() {
-
+		
 		var $container = $( '.container' ),
 		    template   = _.template( $( '#template' ).html() )
 			;
-
-
+		
+		
 		patterns.forEach( function ( pattern, index ) {
 			pattern = (index + 1) + '. '
 			          + pattern[0].toUpperCase() + pattern.slice( 1 );
-
-
+			
+			
 			var content = template( { pattern : pattern, index : index } );
-
+			
 			$container.append(
 				$( content )
 					.find( 'button' )
@@ -35,42 +35,52 @@
 		
 
 		function addScripts( index ) {
-			var $this   = $( this ),
-			    url     = {
+			var $this      = $( this ),
+			    url        = {
 				    pattern     : '/patterns/<%= pattern %>/<%= pattern %>.js',
 				    patternTest : '/patterns/<%= pattern %>/<%= pattern %>.test.js',
 			    },
-			    pattern = patterns[index],
-			    script  = document.createElement( 'script' );
-
-
-			script.src     = _.template( url.pattern )( { pattern : pattern } );
-			script.async   = true;
-			script.onload  = _.partial( addScripts.onLoad, script.src );
-			script.onerror = _.partial( addScripts.onError, script.src );
-			document.body.appendChild( script );
-
-
-			script         = document.createElement( 'script' );
-			script.src     = _.template( url.patternTest )( { pattern : pattern } );
-			script.async   = true;
-			script.onload  = _.compose(
+			    pattern    = patterns[index],
+			    nodeScript = document.createElement( 'script' );
+			
+			
+			nodeScript.src     = _.template( url.pattern )( { pattern : pattern } );
+			nodeScript.async   = true;
+			nodeScript.onload  = _.partial( addScripts.onLoad, nodeScript.src );
+			nodeScript.onerror = _.partial( addScripts.onError, nodeScript.src );
+			document.body.appendChild( nodeScript );
+			
+			
+			nodeScript         = document.createElement( 'script' );
+			nodeScript.src     = _.template( url.patternTest )( { pattern : pattern } );
+			nodeScript.async   = true;
+			nodeScript.onload  = _.compose(
 				function () {
 					$this.off( 'click.addScripts' );
 					$this.click( window.test ).click();
-
+					
 					delete window.test;
 				},
-				_.partial( addScripts.onLoad, script.src )
+				_.partial( addScripts.onLoad, nodeScript.src )
 			);
-			script.onerror = _.partial( addScripts.onError, script.src );
-			document.body.appendChild( script );
+			nodeScript.onerror = _.partial( addScripts.onError, nodeScript.src );
+			document.body.appendChild( nodeScript );
 		}
+		
+		//addScripts.onLoad  = function ( src ) { console.log( '"%s" loaded', src ); };
+		//addScripts.onError = function ( src ) {console.log( 'error on load "%s"', src );};
+		addScripts.onLoad  = function () {};
+		addScripts.onError = function () {};
 
-		addScripts.onLoad  = function ( src ) {console.log( '"%s" loaded', src );};
-		addScripts.onError = function ( src ) {console.log( 'error on load "%s"', src );};
-
+		var hashIndex = location.hash && patterns.indexOf( location.hash.slice( 1 ) );
+		if ( hashIndex && !!~hashIndex ) {
+			$( 'button' )
+				.get( hashIndex )
+				.click()
+			;
+		}
 	}
 }( [
 	   'singleton-with-new',
+	   'singleton-with-getInstance',
    ] ));
